4f927deee1bbc4bf912ef0b7966c30f49f797b3e
Andres <asuarezr2@eafit.edu.co>
2025-03-19 17:56:07 -0500

    Final Commit - Entrega 1

--- prestige/store/admin.py
+++ prestige/store/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Product, Order, OrderItem, User
+from .models import Product, Order, OrderItem, User, Newsletter, Favorite, Address
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Favorite)
+admin.site.register(Address)
+admin.site.register(Newsletter)
 admin.site.register(User, UserAdmin)
